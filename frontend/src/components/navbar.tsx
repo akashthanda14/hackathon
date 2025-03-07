7cc78f5835a1448f9c3beef3fb5c3a2271782a94
akashthanda14 <120628283+akashthanda14@users.noreply.github.com>
2025-03-07 22:59:33 +0530

    Update navbar.tsx

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import React, { useState } from 'react';
+// import axios from 'axios';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 export const Navbar = () => {
